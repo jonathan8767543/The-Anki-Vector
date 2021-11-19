cd9751b0ae5cfded7966441174434a82fc064e04
jonathan8767543 <94583421+jonathan8767543@users.noreply.github.com>
2021-11-19 08:35:28 +0530

    Update__sample_config.py

--- AnkiVector/sample_config.py
+++ AnkiVector/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1812444768  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "DA_RK_SKULL "
+    SUPPORT_CHAT = "impossiblefriendsgroup"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001506289288
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001506289288
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
