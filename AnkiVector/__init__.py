faf77a1c364232256b113a6f38e9b8ce7224b862
jonathan8767543 <94583421+jonathan8767543@users.noreply.github.com>
2021-11-20 14:55:23 +0530

    Update__init__.py

--- AnkiVector/__init__.py
+++ AnkiVector/__init__.py
@@ -181,7 +181,7 @@ else:
     sw = spamwatch.Client(SPAMWATCH_API)
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("ankivector", API_ID, API_HASH)
+telethn = TelegramClient("Darkskull", API_ID, API_HASH)
 pbot = Client("ankivectorPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 tbot = telethn
