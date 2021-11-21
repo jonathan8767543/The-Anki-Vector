d135d2e63b6ff0d3893e30d44235e71841df08ca
jonathan8767543 <94583421+jonathan8767543@users.noreply.github.com>
2021-11-21 09:16:44 +0530

    Update sample_config.py

--- AnkiVector/sample_config.py
+++ AnkiVector/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/AnkiVector/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Darkskull/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -19,7 +19,7 @@ class Config(object):
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1812444768  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "DA_RK_SKULL "
-    SUPPORT_CHAT = "impossiblefriendsgroup"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "DARK_SKULL_SUPPORTGROUP"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001506289288
     )  # Prints any new group the bot is added to, prints just the name and ID.
